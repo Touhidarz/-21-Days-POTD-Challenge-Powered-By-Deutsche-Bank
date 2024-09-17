eb615e44f227834aaf07092df97a7a9e983d582f
Touhid Tamboli <touhidtamboli0314@gmail.com>
2024-09-17 12:21:06 +0530

    day - 6 minimize heigths 2

---
+++ day 6.java
@@ -0,0 +1,22 @@
+class Solution {
+     int getMinDiff(int[] arr, int k) {
+        int n =arr.length;
+        Arrays.sort(arr);
+        
+        int ans = arr[n-1] - arr[0] ;
+        
+        int smallest = arr[0]+k;
+        int largest = arr[n-1]-k;
+        
+        int max=0, min=0;
+        for(int i=0; i<n-1; i++){
+            min = Math.min(smallest, arr[i+1]-k);
+            max = Math.max(largest, arr[i]+k);
+            
+            if(min>0)
+            ans = Math.min(ans, max-min);
+        }
+        return ans;
+
+    }
+}
